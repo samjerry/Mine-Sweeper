1b80cb7029bfaa5879e89f09ae4b151f316eeffa
samjerry <jerry.sam@ziggo.nl>
2018-09-13 13:37:42 +0200

    minesweeper files uploaded
    pushed all file that were made before the github was created

---
+++ Assets/Scripts/Element.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Element : MonoBehaviour {
+
+	public bool _isMine;
+
+	[SerializeField]private Sprite[] _cellTexture;
+	[SerializeField]private Sprite _mineTexture;
+	[SerializeField]private GameObject _wScreen; 
+	// [SerializeField]private 
+
+	private bool _interactable = true;
+
+	void Start() {
+		_wScreen.SetActive(false);
+
+		_isMine = Random.value < 0.15;
+
+		int _x = Mathf.RoundToInt(transform.position.x);
+		int _y = Mathf.RoundToInt(transform.position.y);
+		Grid.elements [_x, _y] = this; 
+	}
+
+	public void loadTexture(int adjecentCount){
+		if (_isMine){
+			GetComponent<SpriteRenderer>().sprite = _mineTexture;
+		} else {
+			GetComponent<SpriteRenderer>().sprite = _cellTexture[adjecentCount];
+		}
+	}
+
+	public bool isCovered(){
+		return GetComponent<SpriteRenderer>().sprite.texture.name == "Default";
+	}
+
+	private void OnMouseUpAsButton() {
+
+		if (_isMine){
+			Grid.uncoverMines();
+			Debug.Log("Lost!");
+		} else {
+			int _x = Mathf.RoundToInt(transform.position.x);
+			int _y = Mathf.RoundToInt(transform.position.y);
+			loadTexture(Grid.adjecentMines(_x, _y));	
+
+			Grid.FFuncover(_x, _y, new bool[Grid._w, Grid._h]);
+
+			if (Grid.isFinished()){
+				_wScreen.SetActive(true);
+				Debug.Log("Win!");
+			}
+		} 
+	}
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour { 
+
+	public static int _w = 10;
+	public static int _h = 13;
+	
+	public static Element[,] elements = new Element[_w, _h];
+
+	public static void uncoverMines(){
+		foreach (Element elem in elements){
+			if (elem._isMine){
+				elem.loadTexture(0);
+			}
+		}
+	}
+
+	public static bool mineAt(int _x, int _y){
+		if (_x >= 0 && _y >= 0 && _x < _w && _y < _h){
+			return elements[_x, _y]._isMine;
+		} 
+		return false;
+	}
+
+	public static int adjecentMines(int _x, int _y){
+		int count = 0;
+
+		if (mineAt(_x, _y+1)) count++;
+		if (mineAt(_x, _y-1)) count++;
+		if (mineAt(_x+1, _y)) count++;
+		if (mineAt(_x-1, _y)) count++;
+		if (mineAt(_x+1, _y+1)) count++;
+		if (mineAt(_x-1, _y+1)) count++;	
+		if (mineAt(_x+1, _y-1)) count++;
+		if (mineAt(_x-1, _y-1)) count++;
+
+		return count; 
+	}
+
+  	  //-----------------------------------------------------------------\\
+     //-------------------Start Flood Fill Algorithm----------------------\\
+	//---------------------------------------------------------------------\\
+
+	public static void FFuncover (int _x, int _y, bool[,] visited){
+		if (_x >= 0 && _y >= 0 && _y < _h && _x < _w){
+			if (visited[_x, _y])
+				return;
+
+			elements[_x, _y].loadTexture(adjecentMines(_x, _y));
+
+			if (adjecentMines(_x, _y) > 0){
+				return;
+			}
+
+			visited[_x, _y] = true;
+
+			FFuncover(_x - 1, _y, visited);
+			FFuncover(_x + 1, _y, visited);
+			FFuncover(_x, _y - 1, visited);
+			FFuncover(_x, _y + 1, visited);
+			FFuncover(_x + 1, _y + 1, visited);
+			FFuncover(_x + 1, _y - 1, visited);
+			FFuncover(_x - 1, _y + 1, visited);
+			FFuncover(_x - 1, _y - 1, visited);
+		}
+	}
+
+	  //-----------------------------------------------------------------\\
+     //--------------------End Flood Fill Algorithm-----------------------\\
+	//---------------------------------------------------------------------\\
+
+	public static bool isFinished(){
+		foreach (Element elem in elements){
+			if (elem.isCovered() && !elem._isMine){
+				return false;
+			}
+		}
+		return true;
+	}
+}
